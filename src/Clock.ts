546e26bae62a7cabf5f34ec34527cda4f04d53a8
duwon1229 <endndnjs2@naver.com>
2021-01-26 22:47:32 +0900

    Create initTime func

--- src/Clock.ts
+++ src/Clock.ts
@@ -1,8 +1,18 @@
 import React from "react";
 
-const getTime = (): void => {
-  const time = Date();
-  console.log(time);
+const initTime = () => {
+  const nowTime = new Date();
+  const nowHours = nowTime.getHours();
+  const nowMinutes = nowTime.getMinutes();
+  const nowSeconds = nowTime.getSeconds();
+
+  return { nowHours, nowMinutes, nowSeconds };
+};
+
+const getTime = () => {
+  let time: string;
+  time = Date();
+  console.log(initTime());
 };
 
 getTime();
